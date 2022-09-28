7366f60d85f4b19c7cf7b58cd202f95ce98d2b3c
unknown <zoevanderschyff13@gmail.com>
2022-09-28 10:01:57 +0100

    first commit

---
+++ src/components/sideBar.vue
@@ -0,0 +1,22 @@
+<template>
+<nav>
+    <v-navigation-drawer v-model="drawer" dark app mini-variant mini-variant-width="100" class="teal">
+<v-list>
+    <v-list-item class="mb-12">
+        <v-list-item-content>
+            <v-icon class="mb-2" large>fas fa-hand-holding-medical</v-icon>
+            <v-list-item-subtitle justify-content-align="center">Medi</v-list-item-subtitle>
+        </v-list-item-content>
+    </v-list-item>
+</v-list>
+    </v-navigation-drawer>
+</nav>
+</template>
+
+<script>
+    export default{
+        data: () =>({
+            drawer: true
+        })
+    }
+</script>
